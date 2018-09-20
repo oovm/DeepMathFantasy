41882a6eb74bcb61a0c6a2d40441b318425535f6
GalAster <galaster@foxmail.com>
2018-09-21 00:29:12 +0800

    函数::3 Image Loss, SAD PSNR and SSIM

---
+++ Packages/Plugins.m
@@ -0,0 +1,28 @@
+(* ::Package:: *)
+(* ::Title:: *)
+(*组件注册(Components)*)
+(* ::Subchapter:: *)
+(*程序包介绍*)
+(* ::Text:: *)
+(*Mathematica Package*)
+(*Created by Mathematica Plugin for IntelliJ IDEA*)
+(*Establish from GalAster's template(v1.3)*)
+(**)
+(* ::Text:: *)
+(*Author:Aster*)
+(*Creation Date:2018-09-11*)
+(*Copyright: Mozilla Public License Version 2.0*)
+(* ::Program:: *)
+(*1.软件产品再发布时包含一份原始许可声明和版权声明。*)
+(*2.提供快速的专利授权。*)
+(*3.不得使用其原始商标。*)
+(*4.如果修改了源代码，包含一份代码修改说明。*)
+(**)
+(* ::Section:: *)
+(*Registry*)
+
+
+(* ::Text:: *)
+(*MXNet*)
+DeepMathFantasy`Servent::usage = "";
+DeepMathFantasy`Servent = DeepMathFantasy`MXNet`Servent$MXNet;

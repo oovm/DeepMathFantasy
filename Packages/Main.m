8b143906f25124fe1b344d090a0844b9bd7ff693
GalAster <galaster@foxmail.com>
2018-09-30 20:48:15 +0800

    插件::GluonCV models downloader

--- Packages/Main.m
+++ Packages/Main.m
@@ -1,18 +1,25 @@
 DeepMathFantasy::usage = "";
+$pyPATH::usage = "";
+$pySession::usage = "";
 Begin["`Private`"];
-$pyPATH = "C:\\ProgramData\\Anaconda3\\python.exe";
+$pyPATH = FileNameJoin[{$HomeDirectory, "Anaconda3", "python.exe"}];
+$pySession := $pySession = StartExternalSession[<|
+	"System" -> "Python",
+	"Executable" -> $pyPATH,
+	"ReturnType" -> "Expression"
+|>];
 (*PackageLoadPacletDependency["NeuralNetworks`"];*)
 (*PackageLoadPacletDependency["MXNetLink`"];*)
 (*PackageExtendContextPath[*)
-	(*{*)
-		(*"Developer`",*)
-		(*"MXNetLink`",*)
-		(*"NeuralNetworks`",*)
-		(*"GeneralUtilities`"*)
-	(*}*)
+(*{*)
+(*"Developer`",*)
+(*"MXNetLink`",*)
+(*"NeuralNetworks`",*)
+(*"GeneralUtilities`"*)
+(*}*)
 (*];*)
 (*SetAttributes[*)
-	(*{ },*)
-	(*{Protected, ReadProtected}*)
+(*{ },*)
+(*{Protected, ReadProtected}*)
 (*];*)
 End[]

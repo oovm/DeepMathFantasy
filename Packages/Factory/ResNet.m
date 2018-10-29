840920c7107d48f29b1b2244bef487da94748a20
GalAster <galaster@foxmail.com>
2018-10-29 23:16:00 +0800

    函数::sr res block

--- Packages/Factory/ResNet.m
+++ Packages/Factory/ResNet.m
@@ -12,12 +12,15 @@ CN[c_, k_, p_ : 1, s_ : 1] := ConvolutionLayer[
 	c, {k, k}, "Biases" -> None,
 	"PaddingSize" -> p, "Stride" -> s
 ];
-ResSampleV2[c_Integer] := Block[
+CN2[c_, k_, p_ : 1, s_ : 1] := ConvolutionLayer[
+	c, {k, k}, "PaddingSize" -> p, "Stride" -> s
+];
+ResResampleV2[c_Integer] := Block[
 	{res},
 	res = NetChain@{
-		BN[], Ramp,
+		BN[], ElementwiseLayer["ReLU"],
 		CN[c, 3, 1, 2],
-		BN[], Ramp,
+		BN[], ElementwiseLayer["ReLU"],
 		CN[c, 3, 1, 1]
 	};
 	NetMerge[{CN[c, 1, 0, 2], res}, Plus, Expand -> All]
@@ -25,17 +28,39 @@ ResSampleV2[c_Integer] := Block[
 ResBasicV2[c_Integer] := Block[
 	{res},
 	res = NetChain@{
-		BN[], Ramp, CN[c, 3],
-		BN[], Ramp, CN[c, 3]
+		BN[], ElementwiseLayer["ReLU"],
+		CN[c, 3, 1, 1],
+		BN[], ElementwiseLayer["ReLU"],
+		CN[c, 3, 1, 1]
 	};
 	NetMerge[res, Plus, Expand -> All]
 ];
 ResBlockV2[c_Integer, n_Integer, head_ : True] := Block[
 	{chain},
 	chain = ConstantArray[ResBasicV2[c], n];
-	If[head, PrependTo[chain, ResSampleV2[c]]];
+	If[head, PrependTo[chain, ResResampleV2[c]]];
 	NetChain@chain
 ];
+
+(*TODO:PRelu for SR*)
+
+ResBasicSR[c_Integer] := Block[
+	{res},
+	res = NetChain@{
+		CN2[c, 3, 1, 1],
+		ParametricRampLayer[],
+		CN2[c, 3, 1, 1]
+	};
+	NetMerge[res, Plus, Expand -> All]
+];
+
+
+
+ResResampleSR[c_Integer, s_Integer] := NetChain@{
+	CN2[c * s^2, 3, 1, 1],
+	PixelShuffleLayer[s],
+	ParametricRampLayer[]
+};
 
 
 (* ::Subsection:: *)

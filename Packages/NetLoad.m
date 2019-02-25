ee1eca90366468721beb2d1640a1feb9c54b3fc1
Aster <galaster@foxmail.com>
2019-02-25 21:17:25 +0800

    函数::NetLoad[New] load python trained params back to net

--- Packages/NetLoad.m
+++ Packages/NetLoad.m
@@ -22,32 +22,31 @@
 (*这里应该填这个函数的介绍*)
 (* ::Section:: *)
 (*函数说明*)
-ExampleFunction::usage = "这里应该填这个函数的说明,如果要换行用\"\\r\"\r就像这样";
+NetLoad::usage = "NetLoad[net,name]";
 (* ::Section:: *)
 (*程序包正体*)
 (* ::Subsection::Closed:: *)
 (*主设置*)
-ExNumber::usage = "程序包的说明,这里抄一遍";
-Begin["`Load`"];
+Begin["`NetLoad`"];
 (* ::Subsection::Closed:: *)
 (*主体代码*)
 Version$NetLoad = "V1.0";
 Updated$NetLoad = "2019-02-06";
 (* ::Subsubsection:: *)
 (*功能块 1*)
-ExampleFunction[1] = "我就是个示例函数,什么功能都没有";
-
-
-
-(* ::Subsubsection:: *)
-(*功能块 2*)
-ExampleFunction[2] = "我就是个示例函数,什么功能都没有";
-
-
+NetLoad[net_, name_] := GeneralUtilities`Scope[
+	init = NeuralNetworks`NetDeinitialize[net];
+	node = Import[name <> "-map.json", "RawJSON"];
+	node = Association[Query[All, #Name -> StringSplit[#Path, "."]&]@node];
+	params = Last@Sort@FileNames[name <> "*.params"];
+	params = MXNetLink`NDArrayImport[params];
+	path = Table[DeleteCases[node[key], "Nodes" | "Arrays"] -> params[key], {key, Keys@params}];
+	NetReplacePart[init, path]
+];
 (* ::Subsection::Closed:: *)
 (*附加设置*)
 SetAttributes[
 	{ },
 	{Protected, ReadProtected}
-]
+];
 End[]
--- Packages/NetSave.m
+++ Packages/NetSave.m
@@ -1,27 +1,8 @@
 (* ::Package:: *)
 (* ::Title:: *)
-(*NetSave(NetSave)*)
+(*NetSave*)
 (* ::Subchapter:: *)
-(*程序包介绍*)
-(* ::Text:: *)
-(*Mathematica Package*)
-(*Created by Mathematica Plugin for IntelliJ IDEA*)
-(*Establish from GalAster's template(v1.3)*)
-(**)
-(* ::Text:: *)
-(*Author:Aster*)
-(*Creation Date:2019-02-06*)
-(*Copyright: Mozilla Public License Version 2.0*)
-(* ::Program:: *)
-(*1.软件产品再发布时包含一份原始许可声明和版权声明。*)
-(*2.提供快速的专利授权。*)
-(*3.不得使用其原始商标。*)
-(*4.如果修改了源代码，包含一份代码修改说明。*)
-(**)
-(* ::Text:: *)
-(*这里应该填这个函数的介绍*)
-(* ::Section:: *)
-(*函数说明*)
+(*Introduce*)
 NetSave::usage = "这里应该填这个函数的说明,如果要换行用\"\\r\"\r就像这样";
 (* ::Section:: *)
 (*程序包正体*)
@@ -30,26 +11,30 @@ NetSave::usage = "这里应该填这个函数的说明,如果要换行用\"\\r\"
 Begin["`NetSave`"];
 (* ::Subsection::Closed:: *)
 (*主体代码*)
-Version$NetSave = "V1.0";
-Updated$NetSave = "2019-02-06";
+DeepMath`Settings`NetSaveRewrite = False;
 (* ::Subsubsection:: *)
 (*功能块 1*)
-NameWriter[head_, path_] := head <> NameWriter[path]
+$NameRewrite = {
+	"Weights" -> "Weight",
+	"Biases" -> "Bias"
+};
+NameWriter[head_, path_] := head <> NameWriter[path];
 NameWriter["."] := "Layer";
-NameWriter[str_String] := If[
-	StringFreeQ[str, "."],
-	str,
-	StringRiffle[Part[StringSplit[StringTrim[str, "."], "."], 2 ;; All ;; 2],
-		"."
-	]
+NameWriter[in_String] := GeneralUtilities`Scope[
+	If[StringFreeQ[in, "."], Return@in];
+	str = DeleteCases[StringSplit[in, "."], "Nodes" | "Arrays"];
+	If[!DeepMath`Settings`NetSaveRewrite, Return@StringRiffle[str, "."]];
+	(*UpperCase can't re-init in stupid python version*)
+	str[[-1]] = str[[-1]] /. $NameRewrite;
+	StringRiffle[ToLowerCase[str], "."]
 ];
 link[name_, path_ -> array_] := GeneralUtilities`Scope[<|
 	"Path" -> path,
 	"Name" -> NameWriter[name, path],
 	"Array" -> array,
 	"Type" -> array["Type"],
-	"Shape" -> Dimensions[array],
-	"MinMax" -> ToString /@ MinMax[Flatten@Normal@array]
+	"Shape" -> Dimensions[array]
+(*"MinMax" -> ToString /@ MinMax[Flatten@Normal@array]*)
 |>];
 (* ::Subsubsection:: *)
 (*MXNetJSON*)
@@ -70,7 +55,8 @@ MXNetJSON[model_NeuralNetworks`NetPlan] := GeneralUtilities`Scope[
 NetSave[net_, name_ : "Net"] := GeneralUtilities`Scope[
 	model = NeuralNetworks`ToNetPlan[net];
 	netMap = Flatten[{
-		link["arg:", #]& /@ Normal@model["WeightArrays"], link["aux:", #]& /@ Normal@model["AuxilliaryArrays"]
+		link["arg:", #]& /@ Normal@model["WeightArrays"],
+		link["aux:", #]& /@ Normal@model["AuxilliaryArrays"]
 	}];
 	ndArray = <|(#Name -> MXNetLink`NDArrayCreate@#Array)& /@ netMap|>;
 	map = SortBy[
@@ -91,5 +77,5 @@ NetSave[net_, name_ : "Net"] := GeneralUtilities`Scope[
 SetAttributes[
 	{ NetSave },
 	{ReadProtected}
-]
+];
 End[]
